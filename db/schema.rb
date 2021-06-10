8b8b59f14f5a5f5e47b82511114a58f1d193574c
Matteo <gnappo.1@live.it>
2021-06-10 01:43:12 -0700

    Initial commit with sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_06_10_082140) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_082140) do
     t.string "name"
     t.float "price"
     t.string "description"
-    t.bigint "category_id", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
