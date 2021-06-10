4c1c27c391a8d4abeda2c660206141ea3ba5eacf
Matteo <gnappo.1@live.it>
2021-06-10 01:39:11 -0700

    Initial commit

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,7 @@
+class ProductSerializer < ActiveModel::Serializer
+    # include FastJsonapi::ObjectSerializer
+    attributes :name, :price, :description, :category, :id
+    
+    belongs_to :category, except: [:created_at, :updated_at]
+  end
+  
